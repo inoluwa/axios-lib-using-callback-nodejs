68f2f7d2190c486266a83e34d4d1a1f38dd5b8b2
inoluwa <naomiadeniran426@gmail.com>
2022-08-01 18:47:30 +0100

    2 files modified, 1 added

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+const axios = require('axios').default;
 // run `node index.js` in the terminal
 
-console.log(`Hello Node.js v${process.versions.node}!`);
+console.log(`Fetch API Node.js v${process.versions.node}!`);
+let arr = [];
+
+// Fetch API is a promise:
+//axios("https://jsonplaceholder.typicode.com/posts")
+//.then(data=>{arr=data; console.log("arr:", arr)});
+axios('https://jsonplaceholder.typicode.com/posts').then((err, res) => {
+  console.log(err);
+});
